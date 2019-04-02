87ae997ee490f2ae73dbea140a1490144f0563b1
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-02 12:20:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,18 +1,16 @@
-
-=begin
 def my_select(collection)
  i = 0
  new_collection = []
  while i < collection.length
    if yield(collection[i]) == true
-     new_collection << yield(collection[i])
+     new_collection << (collection[i])
    end
    i +=1
  end
  new_collection
 end
-=end
-
+###################################
+###################################
 def my_select(array)
 count = 0
 result = []
