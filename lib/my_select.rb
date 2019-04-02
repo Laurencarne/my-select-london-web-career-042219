5afa917b3d6722b2d5854c993e1f7d12e3b49fc3
Lauren Carne <oreocookie@hotmail.co.uk>
2019-04-02 12:20:13 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -9,17 +9,4 @@ def my_select(collection)
  end
  new_collection
 end
-###################################
-###################################
-def my_select(array)
-count = 0
-result = []
 
-while count < array.size
-  if yield(array[count]) == true
-  result.push(array[count])
-end
-  count+=1
-end
-result
-end
